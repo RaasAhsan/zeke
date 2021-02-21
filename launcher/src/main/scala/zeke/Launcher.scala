2c25ee8666d46c3a96ddc2372c9b92cb1c65102a
Raas Ahsan <raas.ahsan@gmail.com>
2021-02-21 04:56:12 -0600

    Fix formatting

--- launcher/src/main/scala/zeke/Launcher.scala
+++ launcher/src/main/scala/zeke/Launcher.scala
@@ -6,23 +6,23 @@ import zeke.nodes._
 import zeke.runtime.ZkFunction
 
 object Launcher {
-    def main(args: Array[String]): Unit = {
-        val frameDescriptor = new FrameDescriptor()
+  def main(args: Array[String]): Unit = {
+    val frameDescriptor = new FrameDescriptor()
 
-        val program = new ZkBlockNode(Array(
-                new SetLocalNode(frameDescriptor.findOrAddFrameSlot("name"), new IntLiteralNode(2)),
-                new SetLocalNode(frameDescriptor.findOrAddFrameSlot("foo"), new FunctionNode(
-                        ZkFunction.fromRootNode(
-                                new ZkRootNode(new IntLiteralNode(1337), new FrameDescriptor())
-                        )
-                )),
-                new GetLocalNode(frameDescriptor.findFrameSlot("name")),
-                new InvokeFunctionNode(new GetLocalNode(frameDescriptor.findFrameSlot("foo")), Array())
-        ))
+    val program = new ZkBlockNode(Array(
+      new SetLocalNode(frameDescriptor.findOrAddFrameSlot("name"), new IntLiteralNode(2)),
+      new SetLocalNode(frameDescriptor.findOrAddFrameSlot("foo"), new FunctionNode(
+        ZkFunction.fromRootNode(
+          new ZkRootNode(new IntLiteralNode(1337), new FrameDescriptor())
+        )
+      )),
+      new GetLocalNode(frameDescriptor.findFrameSlot("name")),
+      new InvokeFunctionNode(new GetLocalNode(frameDescriptor.findFrameSlot("foo")), Array())
+    ))
 
-        val root = new ZkRootNode(program, frameDescriptor)
-        val target = Truffle.getRuntime().createCallTarget(root)
+    val root = new ZkRootNode(program, frameDescriptor)
+    val target = Truffle.getRuntime().createCallTarget(root)
 
-        println(target.call())
-    }
+    println(target.call())
+  }
 }
